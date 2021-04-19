59e95f06e8ede3d9e753e988a1d8beb1fec642ee
Simon Dew <Simon.Dew@couchbase.com>
2021-04-19 16:10:56 +0100

    Rename shell examples as .sh and remove prompt

---
+++ docs/modules/rest-api/examples/query-settings-post-access.sh
@@ -0,0 +1,5 @@
+curl -v -X POST -u Administrator:password \
+http://localhost:8091/settings/querySettings/curlWhitelist \
+-d '{"all_access": false,
+     "allowed_urls": ["https://company1.com"],
+     "disallowed_urls": ["https://company2.com"]}'
